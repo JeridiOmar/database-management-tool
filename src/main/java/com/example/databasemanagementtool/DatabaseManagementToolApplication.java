d369f18f9d48bcd9bc716e20b0bfdfd16b0bd95b
JeridiOmar <50057549+JeridiOmar@users.noreply.github.com>
2022-08-03 15:39:17 +0100

    project init

---
+++ src/main/java/com/example/databasemanagementtool/DatabaseManagementToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.databasemanagementtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseManagementToolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/databasemanagementtool/DatabaseManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.databasemanagementtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
