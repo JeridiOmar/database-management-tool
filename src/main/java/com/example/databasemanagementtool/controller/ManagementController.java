5c7e45a3d5e0d4e693912d5fdf8b4ef1b63eefd3
JeridiOmar <50057549+JeridiOmar@users.noreply.github.com>
2022-08-04 13:46:53 +0100

    create management controller

--- src/main/java/com/example/databasemanagementtool/controller/ManagementController.java
+++ src/main/java/com/example/databasemanagementtool/controller/ManagementController.java
@@ -1,4 +1,15 @@
 package com.example.databasemanagementtool.controller;
 
+import com.example.databasemanagementtool.service.ManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/management")
 public class ManagementController {
+
+    @Autowired
+    ManagementService managementService;
+
 }
